25c6bec655f3a7c4055210b0a9bced5e64ee8efd
Telesphoreo <me@telesphoreo.me>
2020-08-10 01:08:51 -0500

    Update LibsDisguises

--- src/main/java/me/libraryaddict/disguise/BlockedDisguises.java
+++ src/main/java/me/libraryaddict/disguise/BlockedDisguises.java
@@ -3,24 +3,25 @@ package me.libraryaddict.disguise;
 import java.util.Arrays;
 import java.util.List;
 import me.libraryaddict.disguise.disguisetypes.Disguise;
-import static me.libraryaddict.disguise.disguisetypes.DisguiseType.*;
 import me.libraryaddict.disguise.disguisetypes.DisguiseType;
+import static me.libraryaddict.disguise.disguisetypes.DisguiseType.*;
 
-public class BlockedDisguises {
-
+public class BlockedDisguises
+{
     public static final List<DisguiseType> forbiddenDisguises = Arrays.asList(ITEM_FRAME, ENDER_DRAGON, PLAYER, GIANT, GHAST, MAGMA_CUBE, SLIME, DROPPED_ITEM, ENDER_CRYSTAL, AREA_EFFECT_CLOUD, WITHER);
     public static boolean disabled = false;
 
-    public static boolean isAllowed(Disguise disguise) {
+    public static boolean isAllowed(Disguise disguise)
+    {
         return isAllowed(disguise.getType());
-
     }
 
-    public static boolean isAllowed(DisguiseType type) {
-        if (forbiddenDisguises.contains(type)) {
+    public static boolean isAllowed(DisguiseType type)
+    {
+        if (forbiddenDisguises.contains(type))
+        {
             return false;
         }
-
         return true;
     }
 }
